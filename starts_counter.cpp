7d00783bb78d2b8e22a54cb849dbdc0d1d1ffafc
Uliana <ouvt02@mail.ru>
2020-10-13 22:11:38 +0300

    Lock add, with exhisting file

--- starts_counter.cpp
+++ starts_counter.cpp
@@ -3,6 +3,10 @@
 #include <linux/can/error.h>
 #include <error.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/file.h>
+
 
 int main()
 {	
@@ -14,9 +18,21 @@ int main()
         perror("Failed to fork");
         return 1;
     }
-
     
-	FILE* file_counter = fopen(name_of_file, "r");
+    if (child_id == 0)
+        child_id = fork();
+    
+    if (child_id == 0)
+        child_id = fork();
+    
+    
+    int counter_fd = -1;
+    while (counter_fd == -1) 
+        counter_fd = open(name_of_file, O_RDWR);
+
+    flock(counter_fd, LOCK_EX);
+    
+ 	FILE* file_counter = fopen(name_of_file, "r");
     int number_of_starts = 0;
 
     if (file_counter != NULL) 
@@ -27,6 +43,8 @@ int main()
     
     file_counter = fopen(name_of_file, "w");
     fprintf(file_counter, "%d", ++number_of_starts);
+    
+    flock(counter_fd, LOCK_UN);
     
 	return 0;
 }
