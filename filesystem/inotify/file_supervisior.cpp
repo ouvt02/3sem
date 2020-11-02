27b19d47d41b4ffad7999055a9c2c9c9ef442375
ouvt02 <ouvt02@gmail.com>
2020-11-02 12:28:34 +0300

    Working version of file_supervision As argv[1] path to directory is given Handling of event like separate function

--- filesystem/inotify/file_supervisior.cpp
+++ filesystem/inotify/file_supervisior.cpp
@@ -10,28 +10,72 @@
 
 
 char* get_new_pathname(const char* directory_name, const char* file_name);
+void handling_of_event(int file_with_changes);
+
 
 int main(int argc, char* argv[])
 {
-    if (argc == 2)
-        chdir(get_new_pathname(get_current_dir_name(), argv[1]));
-    
-    int file_changes = inotify_init();
-    
-    inotify_add_watch(file_changes, get_current_dir_name(), 
-                          IN_CREATE | IN_DELETE | IN_MOVE);
-        
-    struct inotify_event event;
-    char buf[4096];
-    
-    while (true)
+    if(argc < 2)
     {
-        read(file_changes, buf, 4097);
-        printf("%s\n", event.name);
+        printf("Usage: %s [path to directory]\n", argv[0]);
+        return -1;
     }
     
-    close(file_changes);
-    return 0;
+    int file_with_changes = inotify_init1(IN_NONBLOCK);
+    if(file_with_changes < 0)
+    {
+        perror("Bad return in inotify_init");
+        return -1;
+    }
+    
+    int watching_file = inotify_add_watch(file_with_changes, argv[1],  IN_CREATE | IN_DELETE | IN_MOVE);
+    if(watching_file < 0)
+    {
+        perror("Bad return in inotify_add_watch");
+        return -1;
+    }
+    
+    
+    while(true)
+        handling_of_event(file_with_changes);
+}
+
+
+
+void handling_of_event(int file_with_changes)
+{
+    char buffer[4096];
+    
+    while(true)
+    {
+        ssize_t length_of_event = read(file_with_changes, buffer, sizeof(buffer));
+        if(length_of_event <= 0)
+            break;
+        
+        for(char* pointer = buffer; pointer < buffer + length_of_event; pointer += sizeof(struct inotify_event))
+        {
+            struct inotify_event* event = (struct inotify_event*) pointer;
+            pointer += length_of_event;
+            
+            if(event -> mask & IN_CREATE)
+            {
+                printf("Created: %s\n", event -> name);
+                break;
+            }
+            
+            if(event -> mask & IN_DELETE)
+            {
+                printf("Deleted: %s\n", event -> name);
+                break;
+            }
+            
+            if(event -> mask & IN_CREATE)
+            {
+                printf("Created: %s\n", event -> name);
+                break;
+            }
+        }
+    }
 }
 
 char* get_new_pathname(const char* directory_name, const char* file_name)
